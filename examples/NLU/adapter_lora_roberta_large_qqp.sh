18c6cbc4bd4dfee6f304dff3c26dd6b47121350f
zixinw <zixinw@andrew.cmu.edu>
2021-10-19 02:28:51 -0400

    add adapter for qqp

--- examples/NLU/adapter_lora_roberta_large_qqp.sh
+++ examples/NLU/adapter_lora_roberta_large_qqp.sh
@@ -1,25 +1,28 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
-export output_dir="./adapter_houlsby_roberta_large_mnli"
-python -m torch.distributed.launch --nproc_per_node=1 \
+export output_dir="./adapter_lora_roberta_large_qqp"
+python -m torch.distributed.launch --nproc_per_node=4 \
 examples/text-classification/run_glue.py \
 --model_name_or_path roberta-large \
---task_name mnli \
+--task_name qqp \
 --do_train \
 --do_eval \
 --evaluation_strategy epoch \
 --save_strategy epoch \
---max_seq_length 128 \
---per_device_train_batch_size 32 \
---per_device_eval_batch_size 32 \
+--max_seq_length 512 \
+--per_device_train_batch_size 16 \
+--per_device_eval_batch_size 16 \
 --learning_rate 3e-4 \
---num_train_epochs 5 \
---output_dir output_dir/model \
+--num_train_epochs 10 \
+--output_dir $output_dir/model \
 --overwrite_output_dir \
 --logging_steps 10 \
---logging_dir output_dir/log \
---warmup_ratio 0.1 \
+--logging_dir $output_dir/log \
+--warmup_ratio 0.06 \
+--apply_lora \
+--lora_r 8 \
+--lora_alpha 16 \
 --apply_adapter \
 --adapter_type houlsby \
 --adapter_size 8 \
--- examples/NLU/adapter_pfeiffer_roberta_large_mnli.sh
+++ examples/NLU/adapter_pfeiffer_roberta_large_mnli.sh
@@ -1,8 +1,8 @@
-export num_gpus=8
+export CUDA_VISIBLE_DEVICES=1
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
 export output_dir="./adapter_houlsby_roberta_large_mnli"
-python -m torch.distributed.launch --nproc_per_node=num_gpus \
+python -m torch.distributed.launch --nproc_per_node=1 \
 examples/text-classification/run_glue.py \
 --model_name_or_path roberta-large \
 --task_name mnli \
@@ -15,12 +15,13 @@ examples/text-classification/run_glue.py \
 --per_device_eval_batch_size 32 \
 --learning_rate 3e-4 \
 --num_train_epochs 5 \
---output_dir output_dir/model \
+--output_dir $output_dir/model \
 --overwrite_output_dir \
 --logging_steps 10 \
---logging_dir output_dir/log \
+--logging_dir $output_dir/log \
 --warmup_ratio 0.1 \
 --apply_adapter \
 --adapter_type pfeiffer \
 --adapter_size 16 \
---seed 0
+--seed 0 \
+--report_to all
