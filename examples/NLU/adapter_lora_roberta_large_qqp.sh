b3a1c576e129913f52a2e50e2f7672286dbe5fd5
zixinw <zixinw@andrew.cmu.edu>
2021-10-21 23:07:44 -0400

    changed hyper-parameters

--- examples/NLU/adapter_lora_roberta_large_qqp.sh
+++ examples/NLU/adapter_lora_roberta_large_qqp.sh
@@ -1,10 +1,10 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
-export output_dir="./adapter_lora_roberta_base_qqp"
+export output_dir="./adapter_lora_roberta_large_qqp"
 python -m torch.distributed.launch --nproc_per_node=4 \
 examples/text-classification/run_glue.py \
---model_name_or_path roberta-base \
+--model_name_or_path roberta-large \
 --task_name qqp \
 --do_train \
 --do_eval \
@@ -13,7 +13,7 @@ examples/text-classification/run_glue.py \
 --max_seq_length 512 \
 --per_device_train_batch_size 16 \
 --per_device_eval_batch_size 16 \
---learning_rate 5e-4 \
+--learning_rate 3e-4 \
 --num_train_epochs 10 \
 --output_dir $output_dir/model \
 --overwrite_output_dir \
@@ -22,9 +22,9 @@ examples/text-classification/run_glue.py \
 --warmup_ratio 0.06 \
 --apply_lora \
 --lora_r 16 \
---lora_alpha 8 \
+--lora_alpha 16 \
 --apply_adapter \
---adapter_type houlsby \
+--adapter_type pfeiffer \
 --adapter_size 16 \
 --seed 0 \
 --weight_decay 0.1 \
