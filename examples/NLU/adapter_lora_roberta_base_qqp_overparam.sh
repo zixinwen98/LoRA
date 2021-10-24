ece315941dbb93443bdffaab3abdbe567713b96e
zixinw <zixinw@andrew.cmu.edu>
2021-10-24 12:16:04 -0400

    Update adapter_lora_roberta_base_qqp_overparam.sh

--- examples/NLU/adapter_lora_roberta_base_qqp_overparam.sh
+++ examples/NLU/adapter_lora_roberta_base_qqp_overparam.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1,2
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
 export output_dir="./adapter_lora_roberta_base_qqp"
@@ -10,10 +10,10 @@ python -m torch.distributed.launch --nproc_per_node=3 examples/text-classificati
 --evaluation_strategy epoch \
 --save_strategy epoch \
 --max_seq_length 512 \
---per_device_train_batch_size 16 \
---per_device_eval_batch_size 16 \
---learning_rate 5e-4 \
---num_train_epochs 10 \
+--per_device_train_batch_size 32 \
+--per_device_eval_batch_size 32 \
+--learning_rate 1e-3 \
+--num_train_epochs 20 \
 --output_dir $output_dir/model \
 --overwrite_output_dir \
 --logging_steps 10 \
@@ -21,10 +21,10 @@ python -m torch.distributed.launch --nproc_per_node=3 examples/text-classificati
 --warmup_ratio 0.06 \
 --apply_lora \
 --lora_r 128 \
---lora_alpha 8 \
+--lora_alpha 4 \
 --apply_adapter \
 --adapter_type houlsby \
 --adapter_size 128 \
 --seed 0 \
---weight_decay 0.1 \
+--weight_decay 0.05 \
 --report_to all
