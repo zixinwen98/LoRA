5bd8b2581e395f363fa7fcfcd9cefb831a848f00
zixinw <zixinw@andrew.cmu.edu>
2021-10-28 20:18:48 -0400

    Update adapter_lora_roberta_base_qqp_overparam.sh

--- examples/NLU/adapter_lora_roberta_base_qqp_overparam.sh
+++ examples/NLU/adapter_lora_roberta_base_qqp_overparam.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
 export output_dir="./adapter_lora_roberta_base_qqp"
@@ -12,18 +12,19 @@ python examples/text-classification/run_glue.py \
 --max_seq_length 512 \
 --per_device_train_batch_size 32 \
 --per_device_eval_batch_size 32 \
---learning_rate 7e-4 \
---num_train_epochs 10 \
+--learning_rate 1e-3 \
+--num_train_epochs 20 \
 --output_dir $output_dir/model \
 --overwrite_output_dir \
 --logging_steps 10 \
 --logging_dir $output_dir/log \
---warmup_ratio 0.06 \
+--warmup_ratio 0.1 \
 --apply_lora \
---lora_r 256 \
+--lora_r 128 \
 --lora_alpha 1 \
 --apply_adapter \
 --adapter_type houlsby \
---adapter_size 192 \
---weight_decay 0.1 \
---seed 0 
+--adapter_size 128 \
+--weight_decay 0.15 \
+--seed 0 \
+--report_to all 
