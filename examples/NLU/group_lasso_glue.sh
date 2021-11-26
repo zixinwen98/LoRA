ad02ac3f364360fde7cec7634aa8ffc005b2b327
zixinw <zixinw@andrew.cmu.edu>
2021-11-26 16:43:38 -0500

    Update group_lasso_glue.sh

--- examples/NLU/group_lasso_glue.sh
+++ examples/NLU/group_lasso_glue.sh
@@ -5,7 +5,7 @@ export output_dir="./trial_run_group_lasso_roberta_base_mnli"
 
 for gl_param in 0.1
 do
-for lr in 1e-3
+for lr in 1e-4
 do
 python -m torch.distributed.launch --nproc_per_node=8 \
     examples/group-lasso-text-classification/run_glue.py \
@@ -16,15 +16,15 @@ python -m torch.distributed.launch --nproc_per_node=8 \
 --evaluation_strategy epoch \
 --save_strategy epoch \
 --max_seq_length 128 \
---per_device_train_batch_size 32 \
---per_device_eval_batch_size 32 \
+--per_device_train_batch_size 16 \
+--per_device_eval_batch_size 16 \
 --learning_rate $lr \
 --num_train_epochs 20 \
 --output_dir $output_dir/model \
 --overwrite_output_dir \
 --logging_steps 10 \
 --logging_dir $output_dir/log \
---warmup_ratio 0.1 \
+--warmup_ratio 0.06 \
 --seed 0 \
 --report_to all \
 --glasso_param $gl_param
