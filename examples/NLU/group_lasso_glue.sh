04b5eedcb1fdb802693378378821781ff5f460f7
zixinw <zixinw@andrew.cmu.edu>
2021-11-26 01:46:11 -0500

    Update group_lasso_glue.sh

--- examples/NLU/group_lasso_glue.sh
+++ examples/NLU/group_lasso_glue.sh
@@ -3,7 +3,7 @@ export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/ind
 export PYTHONHASHSEED=0
 export output_dir="./group_lasso_roberta_base_mnli"
 
-for gl_param in 0.007 0.01 0.03 0.05
+for gl_param in 0.03 0.05
 do
 python -m torch.distributed.launch --nproc_per_node=8 \
     examples/group-lasso-text-classification/run_glue.py \
@@ -16,7 +16,7 @@ python -m torch.distributed.launch --nproc_per_node=8 \
 --max_seq_length 128 \
 --per_device_train_batch_size 16 \
 --per_device_eval_batch_size 16 \
---learning_rate 3e-4 \
+--learning_rate 1e-3 \
 --num_train_epochs 20 \
 --output_dir $output_dir/model \
 --overwrite_output_dir \
