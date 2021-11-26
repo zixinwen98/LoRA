0021f99c5164ba976edaac768e8c260051e794c2
zixinw <zixinw@andrew.cmu.edu>
2021-11-26 02:39:10 -0500

    Update group_lasso_glue.sh

--- examples/NLU/group_lasso_glue.sh
+++ examples/NLU/group_lasso_glue.sh
@@ -22,7 +22,7 @@ python -m torch.distributed.launch --nproc_per_node=8 \
 --overwrite_output_dir \
 --logging_steps 10 \
 --logging_dir $output_dir/log \
---warmup_ratio 0.1s \
+--warmup_ratio 0.1 \
 --seed 0 \
 --report_to all \
 --glasso_param $gl_param
