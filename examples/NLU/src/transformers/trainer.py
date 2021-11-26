9e58ce553f93e1dd900b06f2109e633eb0f62b41
zixinw <zixinw@andrew.cmu.edu>
2021-11-25 22:39:34 -0500

    Update trainer.py

--- examples/NLU/src/transformers/trainer.py
+++ examples/NLU/src/transformers/trainer.py
@@ -2325,7 +2325,7 @@ class myTrainer(Trainer):
                 self._total_flos += float(self.floating_point_ops(inputs))
                 
                 # Group Lasso Regularizer
-                for name, param in model.named_parameters():
+                for name, param in model.encoder.layer.named_parameters():
                     if 'attention' not in name and 'dense' in name and 'weight' in name and len(param.data.shape) == 2:
                         tr_loss += self.args.glasso_param * torch.norm(param,dim=1).sum() / np.sqrt(param.shape[0])
                     if epoch == 0 and step == 1:
