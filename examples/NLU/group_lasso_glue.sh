c5c78713595e436755516cb08516c38d59383932
zixinw <zixinw@andrew.cmu.edu>
2021-11-25 22:22:20 -0500

    Update group_lasso_glue.sh

--- examples/NLU/group_lasso_glue.sh
+++ examples/NLU/group_lasso_glue.sh
@@ -2,6 +2,9 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
 export output_dir="./adapter_lora_roberta_base_qqp"
+
+for gl_param in [0.01,0.05,0.1,0.3]
+do
 python -m torch.distributed.launch --nproc_per_node=8 \
     examples/group-lasso-text-classification/run_glue.py \
 --model_name_or_path roberta-base \
@@ -11,8 +14,8 @@ python -m torch.distributed.launch --nproc_per_node=8 \
 --evaluation_strategy epoch \
 --save_strategy epoch \
 --max_seq_length 128 \
---per_device_train_batch_size 8 \
---per_device_eval_batch_size 8 \
+--per_device_train_batch_size 16 \
+--per_device_eval_batch_size 16 \
 --learning_rate 3e-4 \
 --num_train_epochs 5 \
 --output_dir $output_dir/model \
@@ -21,4 +24,6 @@ python -m torch.distributed.launch --nproc_per_node=8 \
 --logging_dir $output_dir/log \
 --warmup_ratio 0.06 \
 --seed 0 \
---report_to all 
+--report_to all \
+--glasso_param $gl_param
+done
